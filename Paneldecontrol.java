b2eb6960ab04eaf5d055e6b47747e3efdda0bb1f
daestrella8 <daestrella8@espe.edu.ec>
2024-05-26 12:05:54 -0500

    Registro de personas con generador de usuarios al azar

--- Paneldecontrol.java
+++ Paneldecontrol.java
@@ -1,60 +1,146 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Paneldecontrol {
 
-   private Persona persona1;
-
-   public Paneldecontrol(Persona persona) {
-      persona1 = persona;
-   }
+   Persona persona = new Persona();
+   Scanner scanner = new Scanner(System.in);
+   private String name;
+   private int n = 0;
+   ArrayList<Persona> personas = new ArrayList<>();
 
    public Paneldecontrol() {
-      persona1 = new Persona();
+
    }
 
-   public void panelIngresoDatos() {
-      Scanner scanner = new Scanner(System.in);
+   public void panelPrincipal() throws IOException, InterruptedException {
+
+      int opt;
+      String option;
+
+      while (true) {
+
+         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+         System.out.println("\tBienvenido al Panel Principal\n");
+         System.out.println("1. Ingresar un Usuario");
+         System.out.println("2. Ver Usuaios Registrados");
+         System.out.println("3. Salir\n");
+
+         System.out.print("[+] Digite su opción: ");
+         opt = scanner.nextInt();
+
+         if (opt == 1) {
+            persona = generarObjeto();
+
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            panelIngresoDatos(persona);
+
+            System.out.println("\n[+] Generando Usuario......\n");
+            Thread.sleep(3 * 1000);
+
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            imprimoDatos(persona);
+
+            System.out.print("\n[+] Usuario Creado Correctamente Desea volver al panel Principal?(s/n): ");
+            option = scanner.nextLine().toLowerCase();
+
+            if (option.equals("s")) {
+
+               panelPrincipal();
+
+            } else if (option.equals("n")) {
+               System.exit(0);
+            }
+
+         } else if (opt == 2) {
+
+            option = "";
+            scanner.nextLine();
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            System.out.println("\t Persona Registradas\n");
+
+            if (personas.size() == 0) {
+               System.out.println("\n[!] No se a registrado Ningun Usuario :( ");
+            } else {
+               for (int i = 0; i < personas.size(); i++) {
+                  imprimoPersonas(personas.get(i));
+               }
+            }
+
+            System.out.print("\n[+] Lista de usuario registrados mostrada, Desea volver al panel principal?(s/n): ");
+            option = scanner.nextLine().toLowerCase();
+
+            if (option.equals("s")) {
+               panelPrincipal();
+
+            } else if (option.equals("n")) {
+               System.exit(0);
+            }
+
+         } else if (opt == 3) {
+            System.out.println("\n[+] Gracias por visitarnos, Tenga un Buen Dia :)\n");
+            System.exit(0);
+         } else {
+
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            System.out.println("[!] Opción Invalida !!!!!");
+            Thread.sleep(3 * 1000);
+            panelPrincipal();
+         }
+
+      }
+   }
+
+   public void panelIngresoDatos(Persona name) {
 
       System.out.println("\t Registro de Usuario \n");
+      scanner.nextLine();
 
       System.out.print("Nombre: ");
-      persona1.setNombre(scanner.nextLine());
+      name.setNombre(scanner.nextLine());
 
       System.out.print("Apellido: ");
-      persona1.setApellido(scanner.nextLine());
+      name.setApellido(scanner.nextLine());
 
       System.out.print("idPersonal: ");
-      persona1.setIdPersonal(scanner.nextLine());
+      name.setIdPersonal(scanner.nextLine());
 
       System.out.print("Digite una contraseña: ");
-      persona1.setpass(scanner.nextLine());
+      name.setpass(scanner.nextLine());
 
-      persona1.generarUsuario();
+      name.generarUsuario();
    }
 
-   public void imprimoDatosFinales(Persona persona1) {
+   public void imprimoDatos(Persona name) {
 
       System.out.println("\tConfirmación de Datos\n");
 
-      System.out.println("Nombre: " + persona1.getNombre());
-      System.out.println("Apellido: " + persona1.getApellido());
-      System.out.println("ID Personal: " + persona1.getIdPersonal() + "\n");
-      System.out.println("Su Usuario es: " + persona1.getUsuario().getUsuario());
-      System.out.println("Su contraseña es: " + persona1.getUsuario().getContrasena());
+      System.out.println("Nombre: " + name.getNombre());
+      System.out.println("Apellido: " + name.getApellido());
+      System.out.println("ID Personal: " + name.getIdPersonal() + "\n");
+      System.out.println("Su Usuario es: " + name.getUsuario().getUsuario());
+      System.out.println("Su contraseña es: " + name.getUsuario().getContrasena());
+      personas.add(name);
 
    }
 
-   public void imprimoDatos() {
+   public void imprimoPersonas(Persona name) {
 
-      System.out.println("\tConfirmación de Datos\n");
+      System.out.println("Nombre: " + name.getNombre());
+      System.out.println("Apellido: " + name.getApellido());
+      System.out.println("ID Personal: " + name.getIdPersonal() + "\n");
+      System.out.println("Su Usuario es: " + name.getUsuario().getUsuario());
+      System.out.println("Su contraseña es: " + name.getUsuario().getContrasena());
 
-      System.out.println("Nombre: " + persona1.getNombre());
-      System.out.println("Apellido: " + persona1.getApellido());
-      System.out.println("ID Personal: " + persona1.getIdPersonal() + "\n");
-      System.out.println("Su Usuario es: " + persona1.getUsuario().getUsuario());
-      System.out.println("Su contraseña es: " + persona1.getUsuario().getContrasena());
+      System.out.println("\n############################################\n");
 
+   }
+
+   public Persona generarObjeto() {
+      name = this.name + this.n;
+      Persona name = new Persona();
+      return name;
    }
 
 }
